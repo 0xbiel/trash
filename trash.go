2d00cfe58ce9de769cb9105b053895419cf857e4
Biel <bielp07@gmail.com>
2020-10-22 09:24:31 -0300

    finish 'createDir()' function.

--- trash.go
+++ trash.go
@@ -5,6 +5,19 @@ import (
   "os"
 )
 
+func createDir() {
+	home := os.Getenv("HOME")
+	dir := home + "/.trash"
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0700)
+		if (err != nil) {
+			panic(err)
+		}
+	}
+}
+
+//@@@ TODO: trash core functionality.
 func trash() {}
 
 func help() {
@@ -14,17 +27,18 @@ func help() {
 |  _|  _| .'|_ -|   |
 |_| |_| |__,|___|_|_|
 
-		-> 0xbiel
-
+		~ 0xbiel
 
 Usage: trash [file] [folder]
   `)
 }
 
 func main() {
-  if (len(os.Args) < 2) {
-	help()
-  } else {
-	trash();
-  }
+	createDir()
+	//@@@ TODO: add "-h" argument.
+	if (len(os.Args) < 2) {
+	  help()
+	} else {
+	  trash();
+	}
 }
